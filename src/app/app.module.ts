a5ae4288bffe3b70367c528bfd7329551352babb
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2021-10-11 15:04:38 -0300

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
-//criaados
+
 import { AppRoutingModule } from './app-routing.module';
 import { TypescriptComponent } from './typescript';
 import { ModulosComponent } from './modulos/modulos.component';
+import { ComponenteComponent } from './componente/componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,//aparentemente nao precisa declarar aqui os componentes criados,funcioa sem
     ModulosComponent,
-    TypescriptComponent
+    TypescriptComponent,
+    ComponenteComponent
    
   ],
   imports: [
